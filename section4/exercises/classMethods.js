d2ef0d4264898a6f44addd74275ff8d5f60b0598
Grace Gardner <graceirisgardner@gmail.com>
2021-09-10 13:08:21 -0600

    Complete class methods exercises

--- section4/exercises/classMethods.js
+++ section4/exercises/classMethods.js
@@ -7,17 +7,17 @@ Complete each task, using the Burrito class below as a starting point.
 Make sure to run the file with node in your command line.
 */
 
-// The burrito should also have a property called `toppings`. It should be assigned to 
+// The burrito should also have a property called `toppings`. It should be assigned to
 // the toppings parameter in the constructor.
 
-// Below/outside of the class declaration, create 3 object instances of a burrito. 
+// Below/outside of the class declaration, create 3 object instances of a burrito.
 // The toppings argument should be an Array of Strings.
 
-// The burrito class should have a method named `changeProtein`. 
+// The burrito class should have a method named `changeProtein`.
 // This method should accept one argument, a String.
 // The method should re-assign this.protein to the value that was passed in.
 
-// Call the `changeProtein` method on a burrito, then log the burrito to verify 
+// Call the `changeProtein` method on a burrito, then log the burrito to verify
 // that the protein has been changed.
 
 // The burrito class should have a method named `addTopping`.
@@ -25,16 +25,44 @@ Make sure to run the file with node in your command line.
 // The method should add a topping to the list of toppings held in state.
 
 // Call the `addTopping` method on two burritos, then log the burritos to verify
-// that the proteins have been changed.
+// that the proteins have been changed. **** I'm assuming this was a typo and that the
+// toppings are supposed to change. ****
 
 class Burrito {
-  constructor(protein, base) {
+  constructor(protein, base, toppings) {
     this.protein = protein;
     this.base = base;
+    this.toppings = toppings;
   }
 
-  // ADD CODE
+  changeProtein(newProtein) {
+    this.protein = newProtein;
+  }
+
+  addTopping(newTopping) {
+    this.toppings = this.toppings.push(newTopping);
+  }
 };
 
-// ADD CODE
+var burrito1 = new Burrito("beef", "rice", ["beans", "cheese", "salsa", "sour cream"]);
+console.log(burrito1);
 
+var burrito2 = new Burrito("chicken", "beans", ["lettuce", "cheese"]);
+console.log(burrito2);
+
+var burrito3 = new Burrito("pork", "rice", ["lettuce", "tomato", "avocado"]);
+console.log(burrito3);
+
+
+burrito3.changeProtein("tofu");
+console.log(burrito3);
+
+burrito1.addTopping("peppers");
+console.log(burrito1);
+
+burrito2.addTopping("guacamole");
+console.log(burrito2);
+
+
+
+// ADD CODE
