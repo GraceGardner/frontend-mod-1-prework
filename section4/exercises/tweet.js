0312fa10fb370fb9c6f09e0d154861a4b9f13106
Grace Gardner <graceirisgardner@gmail.com>
2021-09-10 13:50:40 -0600

    Complete tweet exercise

--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -8,6 +8,43 @@ Create several instances of your Tweet and log them to the console. Make sure th
 tweet object instances _behave_ as expected.
 */
 
+
 class Tweet {
+  constructor(content, timeStamp) {
+    this.author = "Grace";
+    this.content = content;
+    this.timeStamp = timeStamp;
+    this.numberOfLikes = 0;
+    this.comments = [];
+  }
+
+  like() {
+    this.numberOfLikes++;
+  }
+
+  comment(newComment) {
+    this.comments.push(newComment);
+  }
 
 };
+
+var tweet1 = new Tweet("Tweets up in here!", "Sept. 10 2021, 1:28PM");
+tweet1.like();
+tweet1.like();
+tweet1.like();
+tweet1.comment("heck yeah tweets!");
+tweet1.comment("tweets are rad!");
+
+console.log(tweet1);
+
+var tweet2 = new Tweet("Turing is cool!", "Sept. 10 2021, 1:45PM");
+tweet2.like();
+tweet2.comment("heck yeah it is!");
+
+console.log(tweet2);
+
+var tweet3 = new Tweet("I love plants!", "Sept. 10 2021, 1:48PM");
+tweet3.like();
+tweet3.comment("Plants are so cool!");
+
+console.log(tweet3);
