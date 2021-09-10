55d1e5f461c09b11274e70ec1fe95534f8a058eb
Grace Gardner <graceirisgardner@gmail.com>
2021-09-10 14:35:09 -0600

    Add login method

--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -11,7 +11,7 @@ tweet object instances _behave_ as expected.
 
 class Tweet {
   constructor(content, timeStamp) {
-    this.author = "Grace";
+    this.author = "";
     this.content = content;
     this.timeStamp = timeStamp;
     this.numberOfLikes = 0;
@@ -25,10 +25,18 @@ class Tweet {
   comment(newComment) {
     this.comments.push(newComment);
   }
+/// I created this login method, because it made more sense to me
+// to login to an account before being able to assemble a tweet. 
+  login(author) {
+    this.author = author;
+  }
 
 };
 
+
+
 var tweet1 = new Tweet("Tweets up in here!", "Sept. 10 2021, 1:28PM");
+tweet1.login("Grace");
 tweet1.like();
 tweet1.like();
 tweet1.like();
@@ -37,13 +45,17 @@ tweet1.comment("tweets are rad!");
 
 console.log(tweet1);
 
+
+
 var tweet2 = new Tweet("Turing is cool!", "Sept. 10 2021, 1:45PM");
+tweet2.login("Sammie")
 tweet2.like();
 tweet2.comment("heck yeah it is!");
 
 console.log(tweet2);
 
 var tweet3 = new Tweet("I love plants!", "Sept. 10 2021, 1:48PM");
+tweet3.login("Steve");
 tweet3.like();
 tweet3.comment("Plants are so cool!");
 
